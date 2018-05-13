489958f5c1a3c693e67e86986409f491c4470984
Hashnroll <ilya.shchuka@gmail.com>
2018-05-13 20:54:41 +0300

    Added primitive graphics basis

--- Source.cpp
+++ Source.cpp
@@ -1,20 +1,45 @@
 #include "Graph.h"
+#include "Drawing.h"
+#include <SFML\Graphics.hpp>
+
+using namespace sf;
+
+
+void init() {
+	vertices.reserve(MAX_VERTICES);
+}
 
 int main() {
 	Graph g;
-	
-	for (int i=0; i<5;i++)
-		g.addVertex();	
-	
-	g.addEdge(1, 2);
-	g.addEdge(2, 3);
-	g.addEdge(3, 4);
-	g.addEdge(4, 1);
-	g.addEdge(2, 4);
 
-	g.print();
+	init(); //начальная инициализация
 
-	system("pause");
+	while (app.isOpen()) {
+		Event event;
+
+		while (app.pollEvent(event)) {
+			if (event.type == Event::Closed)
+				app.close();
+			
+			if (event.type == sf::Event::MouseButtonPressed)
+			{
+				if (event.mouseButton.button == sf::Mouse::Left)
+				{
+					CircleShape newVertex(VERTEX_RADIUS);
+					newVertex.setOrigin(VERTEX_RADIUS, VERTEX_RADIUS);
+					newVertex.setPosition(event.mouseButton.x, event.mouseButton.y);
+					vertices.push_back(newVertex);
+				}
+			}
+		}
+
+		app.clear(backg);
+
+		//drawGrid();
+		drawGraph();
+		
+		app.display(); 
+	}
 
 	return 0;
 }
