2a5aa704def4742de327386ebb92c4ab9b71c29c
Hashnroll <ilya.shchuka@gmail.com>
2018-06-09 00:15:54 +0300

    Revert "Revert "1)Implemented weighted edges. 2)Implemented more advanced gui.""
    This reverts commit ff1749192f8c3e73613149526c7f1399e321c136.

--- Drawing.h
+++ Drawing.h
@@ -1,139 +1,266 @@
 #pragma once
-//#include <SFML\Graphics.hpp>
 #include "Settings.h"
 #include <vector>
 #include <iterator>
 #include <string>
 #include <algorithm>
 
-typedef sf::Vertex* edgeEnd; //конец ребра - указатель на координаты вершины
+//базовый класс для графического объекта интерфейса(вершин, ребер, кнопок)
+class InterfaceObject {
+protected:
+	bool selected; //активен ли объект?
+	sf::FloatRect bound; //границы графического представления объекта
+	sf::Vertex coord; //координаты графического объекта
+public:
+	virtual void draw() = 0; //виртуальная функция для отрисовки объекта
+	virtual bool contains(sf::Vector2f mouseAppPos) = 0; //виртуальная функция для определения попадания нажатия мыши по элементу
+	virtual std::string getType() = 0; //виртуальная функция для получения типа данного объекта(полезна для определения, на какой именно элемент произведено нажатие
 
-struct vertex {
-	sf::Vertex coord; //координаты для соединения с ребрами
-	sf::CircleShape* circle;
-	sf::FloatRect bound;
-	bool selected;
+	sf::Vector2f getCoord() { //получить координаты объекта
+		return coord.position;
+	}
 
-	int index;
-	sf::Text name;
+	bool isSelected() {
+		return selected;
+	}
 
+	virtual void select() = 0; //выделить объект интерфейса
+
+	virtual void deselect() = 0; //отменить выделение объекта интерфейса
+};
+
+//класс вершины, реализующий InterfaceObject
+class vertex : public InterfaceObject {
+private:
+	sf::CircleShape circle; //графическое представление вершины в виде круга
+	
+	int index; //индекс вершины
+	sf::Text name; //надпись на вершине
+
+public:
 	vertex(int x, int y, int ind) {
 		coord.position.x = x; //координаты для... 
 		coord.position.y = y; //...соединения с ребрами
-		coord.color = sf::Color::Black;
+		coord.color = sf::Color::Black; //цвет ребра
 
-		circle = new sf::CircleShape(VERTEX_RADIUS); //настройка круга
-		circle->setOrigin(VERTEX_RADIUS, VERTEX_RADIUS);
-		circle->setPosition(x, y);
-		circle->setFillColor(vertexColor);
+		circle.setRadius(VERTEX_RADIUS); //настройка графического отображения
+		circle.setOrigin(VERTEX_RADIUS, VERTEX_RADIUS);
+		circle.setPosition(x, y);
+		circle.setFillColor(vertexColor);
 
-		bound = circle->getLocalBounds(); //настройка границы
-		bound.left = circle->getPosition().x - 2*VERTEX_RADIUS;
-		bound.top = circle->getPosition().y - 2*VERTEX_RADIUS;
+		bound = circle.getLocalBounds(); //настройка границы
+		bound.left = circle.getPosition().x - 2*VERTEX_RADIUS;
+		bound.top = circle.getPosition().y - 2*VERTEX_RADIUS;
 		bound.width = bound.height = 4 * VERTEX_RADIUS;
 
 		index = ind;
 
-		name.setFont(vertexNameFont); //настройка названия вершины
+		name.setFont(font); //настройка названия вершины
 		name.setString(std::to_string(index));
 		name.setCharacterSize(VERTEX_NAME_SIZE);
 		name.setOrigin(name.getLocalBounds().left + name.getLocalBounds().width/2.0f, name.getLocalBounds().top + name.getLocalBounds().height / 2.0f);
-		name.setPosition(circle->getPosition());
+		name.setPosition(circle.getPosition());
 
 
 		selected = false;
+	}
+
+	int getIndex() {
+		return index;
 	}
 
 	void select() {
 		selected = true;
-		circle->setFillColor(vertexColorSelected);
+		circle.setFillColor(vertexColorSelected);
 	}
 
 	void deselect() {
 		selected = false;
-		circle->setFillColor(vertexColor);
+		circle.setFillColor(vertexColor);
 	}
 
-	void move(float x, float y) {
-		circle->setPosition(x, y);
+	void move(float x, float y) { //перемещение вершины
+		coord.position.x = x;
+		coord.position.y = y;
 
-		bound = circle->getLocalBounds(); //настройка границы
-		bound.left = circle->getPosition().x -  2 * VERTEX_RADIUS;
-		bound.top = circle->getPosition().y - 2 * VERTEX_RADIUS;
+		circle.setPosition(x, y);
+
+		bound = circle.getLocalBounds(); //настройка границы
+		bound.left = circle.getPosition().x -  2 * VERTEX_RADIUS;
+		bound.top = circle.getPosition().y - 2 * VERTEX_RADIUS;
 		bound.width = bound.height = 4 * VERTEX_RADIUS;
 
 		name.setPosition(x, y);
 
-		coord.position.x = x;
-		coord.position.y = y;
 	}
 
-	/*~vertex() {
-		delete circle;
-	}*/
+	void draw() {
+		app.draw(circle);
+		if (fontLoaded) app.draw(name);
+	}
+
+	bool contains(sf::Vector2f mouseAppPos) override {
+		return bound.contains(mouseAppPos.x, mouseAppPos.y);
+	}
+
+	std::string getType() {
+		return "Vertex";
+	}
 };
 
 std::vector<vertex> vertices; //графическое представление вершин
 
-struct edge {
-	edgeEnd line[2];
+//класс ребра, реализующий InterfaceObject
+class edge : public InterfaceObject { 
+private:
+	vertex *v1, *v2; //указатели на вершины данного ребра
+	sf::Vector2f normal; //нормаль к прямой содержащей данное ребро(для вычисления расстояние до ребра)
 
-	edge(int ind1, int ind2) {
-		line[0] = &(vertices[ind1].coord); 
-		line[1] = &(vertices[ind2].coord);
+	sf::Vertex line[2];//графическое отображение
+	sf::Sprite arrow;
+
+	int weight; //вес ребра
+	sf::Text weightText;//отображение веса
+public:
+	edge(vertex *ptr1, vertex *ptr2, int w) {
+		v1 = ptr1;
+		v2 = ptr2;
+
+		normal.x = v2->getCoord().y - v1->getCoord().y;
+		normal.y = v1->getCoord().x - v2->getCoord().x;
+		
+		line[0].color = line[1].color = edgeColor;
+
+		arrow = arrowTemplate;
+
+		weight = w;
+
+		selected = false;
 	}
 
-	sf::Text weight;
+	std::string getType() {
+		return "Edge";
+	}
 
+	//вернуть вершины данного ребра
+	vertex* getFirstVertex() {
+		return v1;
+	}
+	vertex* getSecondVertex() { 
+		return v2;
+	}
+	
+	bool contains(sf::Vector2f mouseAppPos) override {
+		sf::Vector2f v1Pos = line[0].position = v1->getCoord();
+		sf::Vector2f v2Pos = line[1].position = v2->getCoord();
+		normal.x = v2Pos.y - v1Pos.y;
+		normal.y = v1Pos.x - v2Pos.x;
 
+		int C = -1 * (normal.x*v1->getCoord().x+normal.y*v1->getCoord().y); //параметр C в уравнении прямой
+		int distance = abs(normal.x*mouseAppPos.x+normal.y*mouseAppPos.y+C)/(sqrt(normal.x*normal.x+ normal.y*normal.y));
+
+		return (distance < EDGE_BOUND_VALUE) ? true : false;
+	}
+
+	void draw() {
+		sf::Vector2f v1Pos = line[0].position = v1->getCoord();
+		sf::Vector2f v2Pos = line[1].position = v2->getCoord();
+
+		normal.x = v2Pos.y - v1Pos.y;
+		normal.y = v1Pos.x - v2Pos.x;
+
+		sf::Vector2f l(v2Pos.x-v1Pos.x, v2Pos.y-v1Pos.y); //вектор дуги
+		double alpha = atan((v2Pos.y - v1Pos.y) / (v2Pos.x - v1Pos.x)) * 180.0 / 3.141593; //угол наклона дуги
+		alpha = v2Pos.x < v1Pos.x ? 180 + alpha  : alpha;
+		alpha = v2Pos.y < v1Pos.y ? 360 + alpha : alpha;
+
+		arrow.setOrigin(14,10); //настройка отображения стрелки на дуге
+		arrow.setPosition(v1Pos.x+(2.0/3.0)*l.x, v1Pos.y + (2.0 / 3.0)*l.y);
+		arrow.setRotation(alpha);
+
+		weightText.setFont(font); //настройка названия вершины
+		weightText.setFillColor(sf::Color::Black);
+		weightText.setString(std::to_string(weight));
+		weightText.setCharacterSize(EDGE_TEXT_SIZE);
+		weightText.setOrigin(weightText.getLocalBounds().left + weightText.getLocalBounds().width / 2.0f, weightText.getLocalBounds().top + weightText.getLocalBounds().height / 2.0f);
+		weightText.setPosition(arrow.getPosition().x+20*normal.x/sqrt(normal.x*normal.x+normal.y*normal.y), arrow.getPosition().y + 20*normal.y / sqrt(normal.x*normal.x + normal.y*normal.y));
+
+		app.draw(arrow); //отрисовка стрелки
+		app.draw(line, 2, sf::Lines); //отрисовка дуги
+		app.draw(weightText);
+	}
+
+	void select() {
+		selected = true;
+		line[0].color = line[1].color = edgeColorSelected;
+	}
+
+	void deselect() {
+		selected = false;
+		line[0].color = line[1].color = edgeColor;
+	}
 };
 
 std::vector<edge> edges; //графическое представление ребер
 
+void addVertexToScreen(vertex* v) { //добавить вершину на экран
+	vertices.push_back(*v);
+}
+
+void deleteEdgeFromScreen(edge* e) { //удалить ребро с экрана
+	vector<edge>::iterator it = edges.begin();
+	while (it != edges.end()) { //поиск и удаление ребер, инцидентных данной вершине
+		if (it->getFirstVertex() == e->getFirstVertex()|| it->getSecondVertex() == e->getSecondVertex()) {
+			edges.erase(it);
+			break;
+		}
+		it++;
+	}
+}
+
+vector<edge>::iterator deleteEdgeFromScreen(vector<edge>::iterator it) { //удалить ребро с экрана(через итератор)
+	return edges.erase(it);
+}
+
+void deleteVertexFromScreen(vertex* v) { //удалить вершину с экрана
+	vector<edge>::iterator it = edges.begin();
+	while (it!=edges.end()){ //удалить ребра, инцидентные вершине
+		if (it->getFirstVertex() == v || it->getSecondVertex() == v) {
+			it = deleteEdgeFromScreen(it);
+		}
+		else it++;
+	}
+
+	vertices.erase(vertices.begin() + std::distance(vertices.data(), v)); //удалить вершину из массива вершин
+}
+
+void addEdgeToScreen(edge* e) { //добавить ребро на экран
+	edges.push_back(*e);
+}
+
+
+
 void drawGraph() {
 	for (auto e : edges) {
-		sf::Vertex line[2] = {*(e.line[0]), *(e.line[1])};
-		app.draw(line, 2, sf::Lines);
+		e.draw();
 	}
-	if (vertexNameFontLoaded){
-		for (auto v : vertices) {
-			app.draw(*v.circle);
-			app.draw(v.name);
-		}
+	for (auto v : vertices) {
+		v.draw();
 	}
-	else
-		for (auto v : vertices) app.draw(*v.circle);
 }
 
-std::vector<vertex>::iterator findCollision(sf::Event::MouseButtonEvent press) {
+InterfaceObject* findCollision(sf::Vector2f mouseAppPos) {
 	vector<vertex>::iterator v = vertices.begin();
 	while (v != vertices.end()) {
-		if ((*v).bound.contains(sf::Vector2f(press.x, press.y))) return v;
+		if (v->contains(mouseAppPos)) return v._Ptr;
 		v++;
 	}
-	return vertices.end();
-}
 
-void connectSelectedVertices() {
-	for (auto v : vertices) {
-		if (v.selected) {
-			for (auto w : vertices) {
-				if (v.index != w.index && w.selected) { //v!=w так петель нет
-					edge e(v.index, w.index);
-					edges.push_back(e);
-				}
-			}
-		}
+	vector<edge>::iterator e = edges.begin();
+	while (e != edges.end()) {
+		if (e->contains(mouseAppPos)) return e._Ptr;
+		e++;
 	}
-}
 
-void drawGrid() {
-
-	sf::Vertex line[] =
-	{
-		sf::Vertex(sf::Vector2f(10, 10)),
-		sf::Vertex(sf::Vector2f(150, 150))
-	};
-
-	//app.draw(line, 2, sf::Lines);
+	return nullptr;
 }
--- Source.cpp
+++ Source.cpp
@@ -4,9 +4,13 @@
 #include <iostream>
 
 //Design todo-s
-//TODO #1: make a button for connecting vertices
-//TODO #2: implement function "orderVerticesIndices" cause after removing some vertices their indices are "lost"
-
+// +++ TODO #1: implement edge class in drawing.h
+// +++ TODO #2: implement removing of vertices. if vertex removed, all her edges removed too
+// +++ TODO #3: implement connection of draggingEdge to second vertex if mouse released
+//TODO #4: split up drawing.h to drawing.hpp and drawing.cpp
+//TODO #5: fix vertex removing(edge have ptrs to its vertices and when vertex is removed ptr may point to another vertex which is obviously incorrect)
+//TODO #6: implement menu for input edge weight etc.
+ 
 //Performance todo-s
 //TODO #1: check memory leaks
 //TODO #2: copy constructor for vertex struct(cause it's copied to vector)
@@ -16,24 +20,41 @@ using namespace sf;
 void init() {
 	vertices.reserve(MAX_VERTICES); //установить макс. кол-во вершин
 
-	if (vertexNameFont.loadFromFile("fonts/arial.ttf")) 
-		vertexNameFontLoaded = true;
+	if (font.loadFromFile("fonts/arial.ttf"))  //загрузить шрифт для надписей
+		fontLoaded = true;
+
+	if (arrowTexture.loadFromFile("sprites/arrow.png")) //загрузить спрайт для стрелки на дуге
+		arrowTemplate.setTexture(arrowTexture);
 		 
+	view.reset(sf::FloatRect(0,0,WIDTH,HEIGHT));
 }
 
 int main() {
 	Graph g;
 
-
 	init(); //начальная инициализация
 
-	vector<vertex>::iterator collision; //итератор, указывающий на нажатие мыши
-	bool dragVertex = false; //для drag'n'drop на вершины
+	InterfaceObject *collisionPress = nullptr, *collisionRelease = nullptr; //указатели на элемент интерфейса, на которые попали нажатие и отпускание мыши(вершина, ребро, или кнопка)
+	bool dragVertex = false; //drag'n'drop для вершины
+	bool dragEdge = false; //drag'n'drop для ребра
 	float dx = 0, dy = 0; //смещение drag'n'drop относительно координат нажатия мыши
+	Vector2f mouseWindowSelectVertexStart; //координаты начального нажатия на вершину
+	Vector2i mouseWindowPos; //координаты окна
+	Vector2f mouseAppPos; //координаты внутри приложения
+	Vector2f mousePressPos; //координаты нажатия мыши(используется для регистрации первого нажатия)
+
+	bool mousePressed = false; //зажата ли мышь в данный момент(чтобы не обновлять collisionPress)
+	float mouseTravelledDistance = 0.0f; //расстояние, пройденное мышью
+
+	sf::Vertex draggingEdge[2]; //ребро, которое тянется
+	draggingEdge[0].color = edgeColor;
+	draggingEdge[1].color = edgeColor;
 
 	while (app.isOpen()) {
-		Vector2i mouseWindowPos = Mouse::getPosition(app); //получить координаты окна
-		Vector2f mouseAppPos = app.mapPixelToCoords(mouseWindowPos); //получить координаты приложения(т.к. экран приложения может смещаться за																																пределы окна)
+
+		mouseWindowPos = Mouse::getPosition(app); //получить координаты окна
+		mouseAppPos = app.mapPixelToCoords(mouseWindowPos); //получить координаты приложения(т.к. экран приложения может смещаться за																																пределы окна)
+		//cout << "Window: " << mouseWindowPos.x << " " << mouseWindowPos.y << endl << "App: " << mouseAppPos.x << " " << mouseAppPos.y << endl << endl;
 
 		Event event;
 
@@ -41,83 +62,123 @@ int main() {
 			if (event.type == Event::Closed)
 				app.close();
 
-			//обработка нажатий мыши, по нажатию происходит только drag'n'drop
-			if (event.type == Event::MouseButtonPressed) { 
-				collision = findCollision(event.mouseButton);
+			// Resize window : set new size
+			if (event.type == sf::Event::Resized)
+				view.setSize(event.size.width,event.size.height);
+
+			//обработка нажатий мыши
+			if (event.type == Event::MouseButtonPressed && !mousePressed) { //если еще не нажата
+				if (!mousePressed) {
+					mousePressed = true;
+					mousePressPos = mouseAppPos;
+					collisionPress = findCollision(mouseAppPos);
+				}
 				if (event.mouseButton.button == Mouse::Left)
-					if (collision != vertices.end()) {
-						dragVertex = true;
-						dx = mouseAppPos.x - collision->circle->getPosition().x;
-						dy = mouseAppPos.y - collision->circle->getPosition().y;
+					if (collisionPress != nullptr) { //если попали на какой-то объект
+						if (collisionPress->getType() == "Vertex") {  //если попали на вершину 
+							if (collisionPress->isSelected()) { //если она выделена
+								dx = mouseAppPos.x - collisionPress->getCoord().x; //вычислить смещение
+								dy = mouseAppPos.y - collisionPress->getCoord().y;
+
+								dragVertex = true; //включить drag'n'drop для вершины 
+
+							}
+							else { //если не выделена, drag'n'drop для ребра
+								draggingEdge[0].position = collisionPress->getCoord();
+
+								dragEdge = true;
+							}
+						}
 					}
 			}
 
-			//обработка отпусканий мыши, по отпусканиям происходит добавление/удаление
-			if (event.type == Event::MouseButtonReleased) 
+			//обработка отпусканий мыши
+			if (event.type == Event::MouseButtonReleased) //если кнопка мыши отпущена
 			{
-				if (event.mouseButton.button == Mouse::Left)
+				mousePressed = false;
+				collisionRelease = findCollision(mouseAppPos); //определяем, попадает ли нажатие в какой-то объект
+				if (event.mouseButton.button == Mouse::Left) //если отпущена ЛКМ
 				{
-					if (collision == vertices.end()) { //отпускание не попало на вершину
-						vertex* v = new vertex(event.mouseButton.x, event.mouseButton.y, g.getMaxIndex());
-						vertices.push_back(*v);
-						g.addVertex();
-					}
-					else { //отпускание попало на вершину
-						dragVertex = false; //выключить режим drag'n'drop
+					if (collisionRelease != nullptr) { //если попадание в объект
+						if (collisionRelease->getType() == "Vertex") { //если попадание в вершину
+							if (dragVertex) {
+								dragVertex = false; //если тянули вершину, отменить
+							}
+							if (dragEdge) { //если тянули ребро, отменить и прикрепить его к другой вершине
+								dragEdge = false;
+								int indexFirstVertex = ((vertex*)collisionPress)->getIndex(), indexSecondVertex = ((vertex*)collisionRelease)->getIndex(); //получаем индексы вершин данного ребра
+								if (indexFirstVertex!=indexSecondVertex && !g.connected(indexFirstVertex, indexSecondVertex)) { //если вершины еще не соединены, то соединяем
+									edge e((vertex*)collisionPress, (vertex*)collisionRelease, 1);
+									addEdgeToScreen(&e);
 
-						if (collision->selected == true) { //если вершина активирована, деактивировать только ее
-							collision->deselect();
-						}
-						else { //если вершина деактивирована, активировать ее
-							if (!Keyboard::isKeyPressed(Keyboard::LControl)) { //а все остальные деактивировать, если не зажат Left Ctrl
-								vector<vertex>::iterator it = vertices.begin();
-								while (it != vertices.end()) {
-									if (it->selected == true) it->deselect();
-									it++;
+									g.addEdge(indexFirstVertex, indexSecondVertex,1);
 								}
 							}
-							collision->select();
+							if (mouseTravelledDistance < MOUSE_TRAVEL_THRESHOLD) {
+								if (collisionRelease->isSelected()) collisionRelease->deselect(); //если она выделена, отменить выделение
+								else collisionRelease->select(); //если она не выделена, выделить
+							}
+
+							mouseTravelledDistance = 0.0f; //обнуляем расстояние, пройденное мышью
+						}
+						else if (collisionRelease->getType() == "Edge") {
+							if (collisionRelease->isSelected()) collisionRelease->deselect(); //если ребро выделено, отменить выделение
+							else collisionRelease->select(); //если ребро не выделено, выделить
+						}
+					}
+					else { //если нет попадания в объект
+						if (dragEdge) dragEdge = false; //если тянули ребро, отменяем
+						else {
+							vertex v(mouseAppPos.x, mouseAppPos.y, g.getMaxIndex()); //добавить новую вершину на экран
+							addVertexToScreen(&v);
+
+							g.addVertex(); //добавляем вершину в структуру данных
 						}
 					}
 				}
-				if (event.mouseButton.button == sf::Mouse::Right) {
-					if (collision != vertices.end()) {
-						g.deleteVertex(collision->index);
-						vertices.erase(collision); //TODO: это не очищает память процесса; разобраться, как ее очистить
+
+				if (event.mouseButton.button == sf::Mouse::Right) { //если отпущена ПКМ
+					if (collisionRelease != nullptr) {
+						if (collisionRelease->getType() == "Vertex") { //если попали на вершину
+							g.deleteVertex(((vertex*)collisionRelease)->getIndex()); //удаляем вершину из структуры данных
+
+							deleteVertexFromScreen((vertex*)collisionRelease); //удаляем вершину с экрана
+						}
+						if (collisionRelease->getType() == "Edge") {//если попали на ребро
+							g.deleteEdge(((edge*)collisionRelease)->getFirstVertex()->getIndex(), ((edge*)collisionRelease)->getSecondVertex()->getIndex());
+
+							deleteEdgeFromScreen((edge*)collisionRelease); //удаляем ребро с экрана
+						}
 					}
 				}
 			}
 
-			//обработка нажатий клавиатуры
-			if (event.type == Event::KeyPressed) 
-			{
-				if (event.key.code == Keyboard::Key::Space) {
-					for (auto v : vertices) {
-						if (v.selected) {
-							for (auto w : vertices) {
-								if (v.index != w.index && w.selected) { //v!=w так петель нет
-									if (!g.connected(v.index, w.index)) //проверяем, соединены ли вершины
-										g.addEdge(v.index, w.index);
-								}
-							}
-						}
-					}
-					connectSelectedVertices(); //соединить графически
-				}
+			if (mousePressed) { //если мышь зажата
+				float frameMouseDistanceX = mouseAppPos.x - mousePressPos.x,
+					frameMouseDistanceY = mouseAppPos.y - mousePressPos.y; //вычисляем расстояния, пройденные мышью по x и по y за один кадр
+				mouseTravelledDistance += sqrt(frameMouseDistanceX*frameMouseDistanceX + frameMouseDistanceY * frameMouseDistanceY);
 			}
 
 			if (dragVertex) { //если вершина в режиме drag'n'drop
-				collision->move(mouseAppPos.x - dx, mouseAppPos.y - dy); //переместить ее с учетом смещения
+				((vertex*)collisionPress)->move(mouseAppPos.x - dx, mouseAppPos.y - dy); //переместить ее с учетом смещения
 			}
+			else if (dragEdge) { //если тянем ребро
+				draggingEdge[1].position = mouseAppPos; //перемещаем его конец к мыши
+			}
+
 			g.print(); cout << endl;
 		}
+
+		app.setView(view);
 
 		app.clear(backg);
 
 		//drawGrid();
 		drawGraph(); //отобразить граф
-		
-		app.display(); 
+
+		if (dragEdge) app.draw(draggingEdge, 2, sf::Lines); //отобразить тянущееся ребро
+
+		app.display();
 	}
 
 	return 0;
